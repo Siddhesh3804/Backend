eb09a28d35ba711ac351e0c306e315abd387a93b
unknown <siddheshawasare7@gmail.com>
2026-02-04 15:12:55 +0530

    Backend

---
+++ Day 86/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+app.get('/', (req,res) => {
+    res.send("Server is Running");
+});
+
+app.get('/home', (req,res) => {
+    res.send("This is Home Page");
+});
+
+app.get('/about', (req,res) => {
+    res.send("This is About Page");
+});
+
+app.listen(3000,() => {
+console.log('Server running on port 3000');
+});
